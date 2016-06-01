42c137be86ff7f2f985d23f2fa5688bbbe92454a
Tomas <toumasaldia@gmail.com>
2016-06-01 00:30:23 -0400

    Add Stock entity to Producto.scala
    Missing foreign key to Producto

--- app/models/entities/Producto.scala
+++ app/models/entities/Producto.scala
@@ -1,3 +1,5 @@
 package models.entities
 
 case class Producto(id: Long, producto: String, precioActual: Int, calorias: Int) extends BaseEntity
+
+case class Stock(id: Long, producto: Producto, fecha: java.sql.Timestamp, cantidad: Int) extends BaseEntity
