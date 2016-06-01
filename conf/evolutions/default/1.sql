d2d2eb3db148fd64708a06e856985cc0d032512c
Tomas <toumasaldia@gmail.com>
2016-06-01 02:40:17 -0400

    Add SQL code to first evolution so it creates table Producto

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,6 +2,9 @@
 
 create table "suppliers" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR(254) NOT NULL,"desc" VARCHAR(254) NOT NULL);
 
+create table "Producto" ("id" BIGSERIAL NOT NULL PRIMARY KEY, "producto" VARCHAR(254) NOT NULL, "precio_actual" INTEGER, "calorias" INTEGER )
 # --- !Downs
 ;
 drop table "suppliers";
+
+drop table "Producto"
