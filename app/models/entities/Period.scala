70112f87e2b406d160a147bc531b110e79766dc3
Tomas <toumasaldia@gmail.com>
2016-06-11 00:11:15 -0400

    Add missing inheritance from BaseEntity to Period entities

--- app/models/entities/Period.scala
+++ app/models/entities/Period.scala
@@ -1,8 +1,8 @@
 package models.entities
 
-case class Period(id: Long, startingDate: java.sql.Timestamp, endDate: java.sql.Timestamp, earnings: Int)
+case class Period(id: Long, startingDate: java.sql.Timestamp, endDate: java.sql.Timestamp, earnings: Int) extends BaseEntity
 
 case class ProductDetailByPeriod(id: Long, productId: Long, periodId: Long, numberOfPackages: Int,
-                                 quantityByPackage: Int, buyingPrice: Int, sellingPrice: Int)
+                                 quantityByPackage: Int, buyingPrice: Int, sellingPrice: Int) extends BaseEntity
 
-case class Count(id: Long, periodId: Long, productId: Long, remainingQuantity: Int, date: java.sql.Timestamp)
+case class Count(id: Long, periodId: Long, productId: Long, remainingQuantity: Int, date: java.sql.Timestamp) extends BaseEntity
