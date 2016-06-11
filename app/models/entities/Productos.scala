18c3068620c90fa9a943dec0215cc524cecc241d
Tomas <toumasaldia@gmail.com>
2016-06-10 23:03:52 -0400

    Remove entities Venta, DetalleVenta, Stock, Compra, DetalleCompra and related slick tables

--- app/models/entities/Productos.scala
+++ app/models/entities/Productos.scala
@@ -1,5 +1,3 @@
 package models.entities
 
 case class Producto(id: Long, producto: String, calorias: Int) extends BaseEntity
-
-case class Stock(id: Long, productoId: Long, fecha: java.sql.Timestamp, cantidad: Int) extends BaseEntity
