7ffdccb3bbdd62c6b7610e9346a79df6d93d6255
Tomas <toumasaldia@gmail.com>
2016-06-10 23:05:29 -0400

    Add evolution info to create Product table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,6 +2,10 @@
 
 create table "suppliers" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR(254) NOT NULL,"desc" VARCHAR(254) NOT NULL);
 
+create table "Product" ("id" BIGSERIAL NOT NULL PRIMARY KEY, "product" VARCHAR(254) NOT NULL, "calories" INTEGER );
+
 # --- !Downs
 ;
 drop table "suppliers";
+
+drop table "Product";
