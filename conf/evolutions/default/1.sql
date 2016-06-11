2c81d6bad43f231f48d3da4429ab967c18d25e8c
Tomas <toumasaldia@gmail.com>
2016-06-10 23:54:56 -0400

    Setup evolution sql to create Period table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,8 +4,12 @@ create table "suppliers" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR(254
 
 create table "Product" ("id" BIGSERIAL NOT NULL PRIMARY KEY, "product" VARCHAR(254) NOT NULL, "calories" INTEGER );
 
+create table "Period" ("id" BIGSERIAL NOT NULL PRIMARY KEY, "starting_date" TIMESTAMP NOT NULL, "end_date" TIMESTAMP , "earnings" INTEGER);
+
 # --- !Downs
 ;
 drop table "suppliers";
 
 drop table "Product";
+
+drop table "Period";
