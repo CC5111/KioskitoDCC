f579cb1b7d14272a08eca9b2546d6b60c5d5f296
Tomas <toumasaldia@gmail.com>
2016-06-10 23:57:41 -0400

    Add PeriodsController handling GET /conteos but without action

---
+++ app/controllers/PeriodsController.scala
@@ -0,0 +1,18 @@
+package controllers
+import play.api.mvc._
+import models.daos._
+import javax.inject.{Inject, Singleton}
+
+import models.entities.Product
+import play.api.data._
+import play.api.data.Forms._
+import play.api.Play.current
+
+import play.api.i18n.Messages.Implicits._
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class PeriodsController extends Controller{
+    def periods() = play.mvc.Results.TODO
+}
