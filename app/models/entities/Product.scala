288296714d0791409427034cfdf1eef0f750442a
Tomas <toumasaldia@gmail.com>
2016-08-12 16:39:43 -0300

    Fix Stock model missing inheritance from BaseEntity

--- app/models/entities/Product.scala
+++ app/models/entities/Product.scala
@@ -2,4 +2,4 @@ package models.entities
 
 case class Product(id: Long, product: String, calories: Int, currentPrice: Int) extends BaseEntity
 
-case class Stock(id: Long, productId: Long, stock: Int, date: java.sql.Timestamp)
+case class Stock(id: Long, productId: Long, stock: Int, date: java.sql.Timestamp) extends BaseEntity
