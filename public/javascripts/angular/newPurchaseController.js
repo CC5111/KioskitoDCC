0a1044002dbeca9053a3b68cf2582c1a00711041
Tomas <toumasaldia@gmail.com>
2016-08-12 13:19:05 -0300

    Add form fields for new product in Purchase

--- public/javascripts/angular/newPurchaseController.js
+++ public/javascripts/angular/newPurchaseController.js
@@ -6,5 +6,30 @@ var app = angular.module('KioskitoCaDCCApp', []);
 
 app.controller('newPurchaseCtrl', function($scope){
    $scope.numbers = [1,2,3,4,5,6];
+   $scope.bag = [
+     {
+        name: "",
+        packages: 0,
+        quantityPerPackage: 0,
+        purchasePrice: 0,
+        salePrice: 0
+     },
+   ];
 
+
+   $scope.newProduct = function(){
+        $scope.bag.push(
+             {
+                name: "",
+                packages: 0,
+                quantityPerPackage: 0,
+                purchasePrice: 0,
+                salePrice: 0
+             }
+        )
+   }
+
+   $scope.removeProduct = function (index) {
+       $scope.bag.splice(index, 1);
+   };
 });
