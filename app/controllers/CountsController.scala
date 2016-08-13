e4a8926d2cff5e2dde55a8acac540c9c971b1bb6
Tomas <toumasaldia@gmail.com>
2016-08-13 15:26:27 -0300

    Add example form and form binding creating products

--- app/controllers/CountsController.scala
+++ app/controllers/CountsController.scala
@@ -5,16 +5,33 @@ import play.api.mvc._
 import models.daos._
 import javax.inject.{Inject, Singleton}
 
-import models.entities.CountDetailByProduct
+import models.entities.{CountDetailByProduct, Product}
 import play.api.data._
 import play.api.data.Forms._
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class CountsController @Inject()(countDAO: CountDAO, stockDAO: StockDAO)(implicit ec: ExecutionContext) extends Controller{
+class CountsController @Inject()(countDAO: CountDAO, stockDAO: StockDAO, productDAO: ProductDAO)(implicit ec: ExecutionContext) extends Controller{
+
+
+    case class Products(products: Seq[Product])
+
+    val form = Form(
+        mapping(
+            "products" -> seq(
+                mapping(
+                    "id" -> longNumber,
+                    "product" -> text,
+                    "calories" -> number,
+                    "currentPrice" -> number
+                )(Product.apply)(Product.unapply)
+            )
+        )(Products.apply)(Products.unapply)
+    )
+
     def counts() = Action.async(implicit request =>
         countDAO.getCountsWithEarnings().map { counts =>
             Ok(views.html.counts(counts.toList))
@@ -29,5 +46,18 @@ class CountsController @Inject()(countDAO: CountDAO, stockDAO: StockDAO)(implici
 
     def multiForm() = Action {
         Ok(views.html.multi_form())
+    }
+
+    def addProducts() = Action.async { implicit request =>
+        form.bindFromRequest().fold (
+            formWithErrors => {
+                Future(Ok(views.html.multi_form()))
+            },
+            products => {
+                productDAO.insert(products.products)
+                Future(Redirect(routes.ProductsController.products()))
+
+            }
+        )
     }
 }
