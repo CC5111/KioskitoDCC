de159888e0fa47f150e2815fa647194f6efb52b3
ekauffmann <exploraeniac@gmail.com>
2016-08-14 18:57:03 -0300

    created angular file financesChartController.js. When loaded to html, new purchases tab crushes

---
+++ public/javascripts/angular/financesChartController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ElisaPaz on 11-08-2016.
+ */
+
+var app = angular.module('KioskitoCaDCCApp', ['ui.bootstrap']);
+
+app.controller('financesChartCtrl', function($scope, $http){
+
+});
