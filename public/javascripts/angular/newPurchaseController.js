bea58909be1ff2ab11591016221b71bb06b9532d
Pato285 <pisbej@gmail.com>
2016-08-15 20:56:24 -0300

    Inicio POST nueva compra

--- public/javascripts/angular/newPurchaseController.js
+++ public/javascripts/angular/newPurchaseController.js
@@ -4,35 +4,49 @@
 
 var app = angular.module('KioskitoCaDCCApp', ['ui.bootstrap']);
 
-app.controller('newPurchaseCtrl', function($scope, $http){
-    $http.get('/getProductsNames').success(function(data){
-        $scope.names = data;
+app.controller('newPurchaseCtrl', function ($scope, $http) {
+    $http.get('/getProductsNames').success(function (data) {
+        $scope.products = data;
     });
+    $scope.test = "holi"
+    $scope.bag = [
+        {
+            name: "",
+            id: 0,
+            productId: 0,
+            packages: 0,
+            quantityPerPackage: 0,
+            pricePerPackage: 0,
+            salePrice: 0
+        }
+    ];
+    $scope.form = {
+        purchaseId: 0,
+        products: $scope.bag
+    }
 
-   $scope.bag = [
-     {
-        name: "",
-        packages: 0,
-        quantityPerPackage: 0,
-        purchasePrice: 0,
-        salePrice: 0
-     },
-   ];
 
-
-   $scope.newProduct = function(){
+    $scope.newProduct = function () {
         $scope.bag.push(
-             {
+            {
                 name: "",
+                id: 0,
+                productId: 0,
                 packages: 0,
                 quantityPerPackage: 0,
-                purchasePrice: 0,
+                pricePerPackage: 0,
                 salePrice: 0
-             }
+            }
         )
-   }
+    }
 
-   $scope.removeProduct = function (index) {
-       $scope.bag.splice(index, 1);
-   };
+    $scope.removeProduct = function (index) {
+        $scope.bag.splice(index, 1);
+    };
+
+    $scope.submitForm = function () {
+        console.log("posting data....");
+        $http.post('/compras/crear', JSON.stringify($scope.form)).success(function () {/*success callback*/
+        });
+    };
 });
