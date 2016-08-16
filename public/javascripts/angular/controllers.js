8d040af8b77cdd8e5b8401d91da60212d17cbff1
Tomas <toumasaldia@gmail.com>
2016-08-16 15:24:29 -0300

    Fix NewPurchaseController productId and id swap

--- public/javascripts/angular/controllers.js
+++ public/javascripts/angular/controllers.js
@@ -5,36 +5,57 @@
 var app = angular.module('KioskitoCaDCCApp', ['ui.bootstrap', 'nvd3']);
 
 app.controller('newPurchaseCtrl', function($scope, $http){
-    $http.get('/getProductsNames').success(function(data){
-        $scope.names = data;
-    });
-
-    $scope.bag = [
-        {
-            name: "",
-            packages: 0,
-            quantityPerPackage: 0,
-            purchasePrice: 0,
-            salePrice: 0
-        },
-    ];
-
-
-    $scope.newProduct = function(){
-        $scope.bag.push(
+    $http.get('/getProductsNames').success(function (data) {
+            $scope.products = data;
+        });
+        $scope.bag = [
             {
                 name: "",
+                id: 0,
+                productId: 0,
                 packages: 0,
                 quantityPerPackage: 0,
-                purchasePrice: 0,
+                pricePerPackage: 0,
                 salePrice: 0
             }
-        )
-    }
+        ];
+        $scope.form = {
+            purchaseId: 0,
+            products: $scope.bag
+        }
 
-    $scope.removeProduct = function (index) {
-        $scope.bag.splice(index, 1);
-    };
+
+        $scope.newProduct = function () {
+            $scope.bag.push(
+                {
+                    name: "",
+                    id: 0,
+                    productId: 0,
+                    packages: 0,
+                    quantityPerPackage: 0,
+                    pricePerPackage: 0,
+                    salePrice: 0
+                }
+            )
+        }
+
+        $scope.removeProduct = function (index) {
+            $scope.bag.splice(index, 1);
+        };
+        $scope.findProductId = function(n){
+            for (var i = 0; i < $scope.products.length; i++) {
+                if ($scope.products[i].name == n) return $scope.products[i].id
+            }
+        }
+        $scope.submitForm = function () {
+            for (var i = 0; i < $scope.bag.length; i++) {
+                $scope.bag[i].productId = $scope.findProductId($scope.bag[i].name)
+            }
+            console.log(JSON.stringify($scope.form));
+            console.log($scope.form);
+            $http.post('/compras/crear', $scope.form).success(function () {/*success callback*/
+            });
+        };
 });
 
 app.controller('financesChartCtrl', function($scope){
