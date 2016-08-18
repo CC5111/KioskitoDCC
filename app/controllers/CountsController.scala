9f12ee7b3c7b8f7826c5660aaa5a4d645a755028
Tomas <toumasaldia@gmail.com>
2016-08-18 17:58:39 -0300

    Remove old form in CountsController and old method

--- app/controllers/CountsController.scala
+++ app/controllers/CountsController.scala
@@ -21,26 +21,6 @@ import implicits.JsonWrites.{caloriesPerCountReads}
 @Singleton
 class CountsController @Inject()(countDAO: CountDAO, countDetailDAO: CountDetailByProductDAO, stockDAO: StockDAO)(implicit ec: ExecutionContext) extends Controller{
 
-    val countsForm = Form(
-        mapping (
-            "countId" -> longNumber,
-            "countDetails" -> seq(
-                mapping(
-                    "id" -> longNumber,
-                    "productId" -> longNumber,
-                    "remainingQuantity" -> number,
-                    "soldQuantity" -> number,
-                    "salePrice" -> number
-                )({
-                    case (id, productId, remaining, sold, salePrice) => CountDetailByProduct(id, 0, productId, remaining, sold, salePrice)
-                })
-                ({
-                    case detail : CountDetailByProduct => Some((detail.id, detail.productId, detail.quantity, detail.soldQuantity, detail.sellingPrice))
-                })
-            )
-        )(CountDetails.apply)(CountDetails.unapply)
-    )
-
     def counts() = Action.async(implicit request =>
         countDetailDAO.getCountsWithEarnings().map { counts =>
             Ok(views.html.counts(counts.toList))
@@ -53,7 +33,7 @@ class CountsController @Inject()(countDAO: CountDAO, countDetailDAO: CountDetail
         }
     )
 
-    def createCountsFromJSON = Action.async(BodyParsers.parse.json) { implicit request =>
+    def createCounts = Action.async(BodyParsers.parse.json) { implicit request =>
         val countResult = request.body.validate[CountDetails]
 
         countResult.fold (
@@ -82,32 +62,6 @@ class CountsController @Inject()(countDAO: CountDAO, countDetailDAO: CountDetail
 
         )
     }
-
-    def createCounts = Action.async(implicit request =>
-        countsForm.bindFromRequest().fold (
-            formWithErrors => {
-                Future(Redirect(routes.CountsController.counts()))
-            },
-            countDetails => {
-                val calendar = Calendar.getInstance()
-                val currentDate = calendar.getTime
-                val currentTimestamp: Timestamp = new Timestamp(currentDate.getTime)
-
-                countDAO.insert(Count(countDetails.countId, currentTimestamp, 0)).map {
-                    insertedCountId =>
-                        countDetails.countDetails.map(
-                            countDetail => {
-                                countDetailDAO.insert(countDetail.copy(countId = insertedCountId))
-                                stockDAO.insert(Stock(0, countDetail.productId, countDetail.quantity, currentTimestamp))
-                            }
-                        )
-                }
-
-                Future(Redirect(routes.CountsController.counts()))
-
-            }
-        )
-    )
 
     def countsTotalCalories = Action.async{ implicit request =>
             countDAO.totalCaloriesPerCount.map{calories =>
